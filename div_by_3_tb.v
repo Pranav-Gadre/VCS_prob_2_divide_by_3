1aa9dd1549e271ebd1c7166651c3fbffeb89711f
Cb_cat <leapofleopard@gmail.com>
2025-09-08 01:10:14 +0530

    commit 5: adding a TB file.

---
+++ div_by_3_tb.v
@@ -0,0 +1,63 @@
+`timescale 1ns/1ps
+
+module div_by_3_tb();
+    
+    parameter DL105 = 105,
+              DL200 = 200;
+    reg clk,
+        reset,
+        x_i;
+    wire div_o;
+    integer i;
+    
+    initial begin 
+    reset <= 1;
+    #50; 
+    reset <= 0;
+    end 
+    
+    initial begin 
+    clk = 0;
+    x_i   = 0;
+    end 
+    
+    always #5 clk = ~clk;
+    
+    task par_to_ser_in(
+        input [15:0] number
+        );
+        begin 
+            for (i = 0; i<=15; i=i+1) begin
+                @(posedge clk);
+                x_i <= number[i];
+            end 
+        end 
+    endtask
+    
+    initial begin 
+        #DL105;
+        par_to_ser_in(3);
+        #DL200;
+        par_to_ser_in(15);
+        #DL200;
+        par_to_ser_in(75);
+        #DL200;
+        par_to_ser_in(375);
+        #DL200;
+        par_to_ser_in(47);
+    end 
+    
+    property sync_resetn;
+        @(posedge clk) disable iff (reset)
+        ($fell(x_i) | $rose(x_i)) |-> ($fell(div_o) | $fell(div_o));
+    endproperty
+    
+    div_by_three dbt0(
+    .clk      (    clk      ),
+    .reset    (    reset    ),
+    .x_i      (    x_i      ),
+    .div_o    (    div_o    )
+    
+    );  
+
+endmodule
